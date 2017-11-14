d5c783008a74e236eb05688da797d8c00f62d8c4
zamozd <zz.meddour@gmail.com>
2017-11-14 17:08:18 +0100

    add functions on the main

--- main.py
+++ main.py
@@ -5,6 +5,41 @@ import sys
 import socket
 import argparse
 
+def checkConnectionAndReturnSocket( Hostname, Port ):
+    s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    connection = s.connect( (socket.gethostbyname( Hostname ), Port) )
+
+    if connection == True:
+        return s
+    else:
+        return False
+
+    def checkByUsernameFile( F, Hostname, Port, numLines ):
+        s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        s.connect( (socket.gethostbyname( Hostname ), Port) )
+        f = open( F, 'r' )
+        if f:
+        i = 0
+        while i != numLines:
+            user = f.readline().strip()
+            s.send( 'VRFY '.encode() + user.encode() + ' \r\n'.encode() )
+            usercheck = s.recv( 1024 )
+            if '250'.encode() in usercheck:
+                print( '[*] {} exist on the mail server'.format( user ) )
+            else:
+            print( '[-] {} doesn\'t exist on the mail server'.format( user ) )
+            i = i + 1
+
+    def checkTheUser( user, Hostname, Port ):
+        s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        s.connect( (socket.gethostbyname( Hostname ), Port) )
+        s.send( 'VRFY '.encode() + user.encode() + ' \r\n'.encode() )
+        usercheck = s.recv( 1024 )
+        if '250'.encode() in usercheck:
+            print( '[*] {} exist on the mail server'.format( user ) )
+        else:
+            print( '[-] {} doesn\'t exist on the mail server'.format( user ) )
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument( '--username',  type=str,   help='put single username here' )
@@ -14,6 +49,19 @@ def main():
     parser.add_argument( '--num',       type=int,   help='put the number of usernames to check' )
     args = parser.parse_args()
 
+    if args.hostname:
+        s = checkConnectionAndReturnSocket( args.hostname, args.p )
+        print( '[*] the server is alive and port 25 is open' )
+    else:
+        print( '[-] the server is not alive or the tcp port is not open' )
+        sys.exit(1)
+    if args.u:
+        checkUsername = checkTheUser( args.u, args.hostname, args.p )
+    elif args.U:
+        if args.num is None:
+            print( 'you have to specify the number of checks' )
+    else:
+        checkByUsernameFile( args.U, args.hostname, args.p, args.num )
 
 if __name__ == "__main__":
             main()
