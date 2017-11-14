a019589ed69bc406a2f1fa4cad9cfc17205e2832
zamozd <zz.meddour@gmail.com>
2017-11-14 17:52:11 +0100

    add the else -h option

--- main.py
+++ main.py
@@ -65,7 +65,8 @@ def main():
     elif args.usernames:
         checkByUsernameFile( args.usernames, args.hostname, args.port, args.num )
     else:
-        print( 'something is wrong here' )
+        print( 'something is wrong here\n
+                use the -h option and see' )
 
 if __name__ == "__main__":
             main()
