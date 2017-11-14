6b2abf1ad7c9798802763c13c916408cb009032e
zamozd <zz.meddour@gmail.com>
2017-11-14 17:05:55 +0100

    add essential functions

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import sys
 import socket
 import argparse
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '--username',  type=str,   help='put single username here' )
+    parser.add_argument( '--usernames',             help='put file of usernames' )
+    parser.add_argument( '--port',      type=int,   help='put the tcp port here' )
+    parser.add_argument( '--hostname',  type=str,   help='put here the hostname of the smtp server' )
+    parser.add_argument( '--num',       type=int,   help='put the number of usernames to check' )
+    args = parser.parse_args()
+
+
 if __name__ == "__main__":
             main()
 
